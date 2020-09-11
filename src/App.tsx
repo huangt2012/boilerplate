a7cdedea24bb0ade27380e61916da4b235b61d45
huangt2012 <huangt2012@163.com>
2020-09-11 17:40:34 +0800

    :tada: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { Home, Detail } from 'containers'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/detail">
+        <Detail />
+      </Route>
+    </Switch>
+  )
 }
 
-export default App;
+export default App
